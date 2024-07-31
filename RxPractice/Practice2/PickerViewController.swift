e3267c9bcd31541ad07902e2a4d303e5ea2194ba
YeoSungeun <dutjddms@naver.com>
2024-08-01 07:45:40 +0900

    [Feat] simpleTableView practice

--- RxPractice/Practice2/PickerViewController.swift
+++ RxPractice/Practice2/PickerViewController.swift
@@ -5,4 +5,58 @@
 //  Created by 여성은 on 8/1/24.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
+
+class PickerViewController: UIViewController {
+    let simplePickerView = UIPickerView()
+    let simpleLabel = UILabel()
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        setPickerView()
+    }
+    func setView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(simplePickerView)
+        view.addSubview(simpleLabel)
+        
+        simplePickerView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(44)
+        }
+        simpleLabel.snp.makeConstraints { make in
+            make.top.equalTo(simplePickerView.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(44)
+        }
+        simplePickerView.backgroundColor = .systemYellow
+        simpleLabel.backgroundColor = .systemBlue
+    }
+    func setPickerView() {
+        let items = Observable.just([
+            "영화",
+            "드라마",
+            "애니메이션",
+            "기타"
+        ])
+        items
+            .bind(to: simplePickerView.rx.itemTitles) { (row, elemnet) in
+                return elemnet
+            }
+            .disposed(by: disposeBag)
+        
+        simplePickerView.rx.modelSelected(String.self)
+            .map{ $0.description }
+            .bind(to: simpleLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+   
+    
+}
+
--- RxPractice/Practice2/SampleTableViewController.swift
+++ RxPractice/Practice2/SampleTableViewController.swift
@@ -5,4 +5,63 @@
 //  Created by 여성은 on 8/1/24.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
+
+class SampleTableViewController: UIViewController {
+    let simpleTableView = UITableView()
+    let simpleLabel = UILabel()
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        setTableView()
+    }
+    func setView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(simpleLabel)
+        view.addSubview(simpleTableView)
+        
+
+        simpleLabel.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(44)
+        }
+        simpleTableView.snp.makeConstraints { make in
+            make.top.equalTo(simpleLabel.snp.bottom).offset(20)
+            make.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        simpleTableView.backgroundColor = .systemYellow
+        simpleLabel.backgroundColor = .systemBlue
+    }
+    func setTableView() {
+        simpleTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        let items = Observable.just([
+            "First Item",
+            "Second Item",
+            "Third Item"
+        ])
+        items
+            .bind(to: simpleTableView.rx.items) { (tableView, row, element) in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+                cell.textLabel?.text = "\(element) @ row \(row)"
+                
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        simpleTableView.rx.modelSelected(String.self)
+            .map{ data in
+                "\(data)를 클릭했습니다."
+            }
+            .bind(to: simpleLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+   
+   
+    
+}
